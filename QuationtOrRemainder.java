f6b12cc1dc73e9d28170396cbefe28a6ab459cfb
Ankita Anil Saurkar <80325186+ankitasaurkar@users.noreply.github.com>
2021-06-22 17:17:38 +0530

    Quationt or Remainder

---
+++ QuationtOrRemainder.java
@@ -0,0 +1,12 @@
+public class QuotientRemainder
+ {
+ public static void main(String args[])
+   {
+  int divisor=5;
+  int divident=25;
+  int quotient=divident/divisor;
+  int remainder=divident%divisor;
+System.out.println("Quotient is = "+quotient);
+System.out.println("Remainder is = "+remainder);
+   }
+}
