f3aec545de82603137ecf1e6e8cf9537aed8c487
Ankita Anil Saurkar <80325186+ankitasaurkar@users.noreply.github.com>
2021-06-22 17:13:10 +0530

    SwitchProgram

---
+++ SwitchProgram.java
@@ -0,0 +1,20 @@
+public class SwitchProgram
+{
+public static void main(String args[])
+{
+int a=5,b=6;
+int result;
+result=a+b;
+switch(case)
+{
+case 0:result=10;
+break;
+case 1:result=11;
+break;
+case 3:result=12;
+break;
+default:System.out.println("Error Occured");
+System.exit(0);
+}
+}
+} 
