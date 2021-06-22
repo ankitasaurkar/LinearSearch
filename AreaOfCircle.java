64df28cef27bc35253fad23a9be3c0feca4dbaf0
Ankita Anil Saurkar <80325186+ankitasaurkar@users.noreply.github.com>
2021-06-22 17:24:31 +0530

    Area of Circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+public class AreaOfCircle
+{
+ public static void main(String args[])
+  {
+  double r=2;
+  double Area;
+ System.out.println("The Radius is :"+r);
+  Area=r*r*Math.PI;
+ System.out.println("Area of circle is: "+Area);
+  }
+}
